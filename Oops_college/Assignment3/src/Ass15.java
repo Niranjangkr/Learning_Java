c37f3232c6b5c918a235e82ff39c68b34b3966a3
Niranjangkr <niranjangaonkar.09@gmail.com>
2022-10-27 05:24:58 +0530

    Ass3_15

---
+++ Oops_college/Assignment3/src/Ass15.java
@@ -0,0 +1,11 @@
+/*Assign No 15 
+A company has 5 departments namely ‘Production’, ‘HR’,’Sales’,Advertisement’, ‘Accounts’. Each department has dno and some budgetary allocation besides the name. Using the collection framework, write a Java program which stores the department details and also lists the departments in the descending order of their budget allocation and also searches and displays the department given its dno.
+*/
+public class Ass15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
